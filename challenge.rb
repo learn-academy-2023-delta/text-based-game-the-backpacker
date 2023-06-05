e72b6ce005f422cd09f5963aa848d8e50bbd3227
Chris-Frey <chris.frey@live.com>
2023-06-05 11:27:36 -0700

    add pseudo-code

--- challenge.rb
+++ challenge.rb
@@ -1,1 +1,14 @@
-# Text-based Game Challenge
+# ask to enter name of character (gets.chomp)
+# they enter name
+# save the name to variable
+# points left variable - counter 
+# total points variable (from multiple games)
+
+# set up methods
+#   - the prompt showing that piece of the story
+#   - can you guess where name is? y/n 
+#     if y - gets.chomp what country do you think it is?
+#         if correct - yes, you get x points
+#         else no - try again after the next clue
+#       n - go to next clue
+  
